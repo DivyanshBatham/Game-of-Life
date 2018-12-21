fe140d38672e67d1ddc3b12cf1a890fce11058aa
Divyansh Batham <divyansh.sunita@gmail.com>
2018-12-21 13:40:47 +0530

    Updates

---
+++ precache-manifest.6290505aa31e7b4821c5ea93890c8a3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc5a2e2a365b794b607655d505bfd2bd",
+    "url": "/Game-of-Life/static/media/logo.cc5a2e2a.png"
+  },
+  {
+    "revision": "fd4c1304f70260e2c289e0e016a356e6",
+    "url": "/Game-of-Life/static/media/logo-mobile.fd4c1304.png"
+  },
+  {
+    "revision": "a07be2c9bc3792a5807f",
+    "url": "/Game-of-Life/static/js/runtime~main.a07be2c9.js"
+  },
+  {
+    "revision": "733ea5af88bc2d7ff21d",
+    "url": "/Game-of-Life/static/js/main.733ea5af.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Game-of-Life/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "733ea5af88bc2d7ff21d",
+    "url": "/Game-of-Life/static/css/main.6810100b.chunk.css"
+  },
+  {
+    "revision": "2aadde329030b155c3f01be33ac57f9b",
+    "url": "/Game-of-Life/index.html"
+  }
+];
